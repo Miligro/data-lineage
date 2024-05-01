736b26d8712a40ca3af3a9e02b73f6b3684557f4
Tobias2705 <t_gruszczynski@op.pl>
2024-05-01 19:14:10 +0200

    Fix oracle lineage

--- Metadata/oracle_metadata.py
+++ Metadata/oracle_metadata.py
@@ -9,12 +9,14 @@ class OracleDatabaseMetadata:
         self.user = user
         self.password = password
         self.conn = None
-
-    def connect(self):
+        self.init_oracle_client()
+    
+    def init_oracle_client(self):
         base_dir = os.path.dirname(os.path.abspath(__file__))
         oracle_client_dir = os.path.join(base_dir, "instantclient_19_9")
         cx_Oracle.init_oracle_client(lib_dir=oracle_client_dir)
 
+    def connect(self):
         dsn_tns = cx_Oracle.makedsn(self.host, self.port, service_name=self.dbname)
         self.conn = cx_Oracle.connect(user=self.user, password=self.password, dsn=dsn_tns)
         print("Connected to the database - Oracle.")
@@ -37,7 +39,10 @@ class OracleDatabaseMetadata:
                 FROM
                     all_tab_columns
                 WHERE
-                    owner NOT IN ('SYS', 'SYSTEM')
+                    NOT (owner LIKE '%SYS%' OR owner LIKE 'SYS%' OR owner LIKE '%SYS')
+                    AND NOT (owner LIKE '%ADMIN%' OR owner LIKE 'ADMIN%' OR owner LIKE '%ADMIN')
+                    AND NOT (owner LIKE '%DBS%' OR owner LIKE 'DBS%' OR owner LIKE '%DBS')
+                    AND owner NOT IN ('OUTLN', 'XDB', 'REMOTE_SCHEDULER_AGENT')
             """)
             return cur.fetchall()
 
@@ -55,7 +60,10 @@ class OracleDatabaseMetadata:
                     JOIN all_constraints ac ON acc.constraint_name = ac.constraint_name
                 WHERE
                     ac.constraint_type IN ('P', 'R', 'U')
-                    AND acc.owner NOT IN ('SYS', 'SYSTEM')
+                    AND NOT (acc.owner LIKE '%SYS%' OR acc.owner LIKE 'SYS%' OR acc.owner LIKE '%SYS')
+                    AND NOT (acc.owner LIKE '%ADMIN%' OR acc.owner LIKE 'ADMIN%' OR acc.owner LIKE '%ADMIN')
+                    AND NOT (acc.owner LIKE '%DBS%' OR acc.owner LIKE 'DBS%' OR acc.owner LIKE '%DBS')
+                    AND acc.owner NOT IN ('OUTLN', 'XDB', 'REMOTE_SCHEDULER_AGENT')
             """)
             return cur.fetchall()
 
@@ -71,7 +79,10 @@ class OracleDatabaseMetadata:
                     all_dependencies
                 WHERE
                     type = 'VIEW'
-                    AND owner NOT IN ('SYS', 'SYSTEM')
+                    AND NOT (owner LIKE '%SYS%' OR owner LIKE 'SYS%' OR owner LIKE '%SYS')
+                    AND NOT (owner LIKE '%ADMIN%' OR owner LIKE 'ADMIN%' OR owner LIKE '%ADMIN')
+                    AND NOT (owner LIKE '%DBS%' OR owner LIKE 'DBS%' OR owner LIKE '%DBS')
+                    AND owner NOT IN ('OUTLN', 'XDB', 'REMOTE_SCHEDULER_AGENT')
             """)
             return cur.fetchall()
 
@@ -88,6 +99,9 @@ class OracleDatabaseMetadata:
                     all_source
                 WHERE
                     type = 'PROCEDURE'
-                    AND owner NOT IN ('SYS', 'SYSTEM')
+                    AND NOT (owner LIKE '%SYS%' OR owner LIKE 'SYS%' OR owner LIKE '%SYS')
+                    AND NOT (owner LIKE '%ADMIN%' OR owner LIKE 'ADMIN%' OR owner LIKE '%ADMIN')
+                    AND NOT (owner LIKE '%DBS%' OR owner LIKE 'DBS%' OR owner LIKE '%DBS')
+                    AND owner NOT IN ('OUTLN', 'XDB', 'REMOTE_SCHEDULER_AGENT')
             """)
             return cur.fetchall()
