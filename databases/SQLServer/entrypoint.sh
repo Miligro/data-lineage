93869faad5d82e5cf552217d98000e303cd8b0af
Miligro <milo20054@gmail.com>
2024-08-20 22:25:33 +0200

    Fix sqlserver entrypoint

--- databases/SQLServer/entrypoint.sh
+++ databases/SQLServer/entrypoint.sh
@@ -3,11 +3,11 @@
 /opt/mssql/bin/sqlservr &
 
 echo "Waiting for SQL Server to start..."
-until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Sqlserver123" -Q "SELECT 1" > /dev/null 2>&1; do
+until /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Sqlserver123" -C -Q "SELECT 1" > /dev/null 2>&1; do
     echo "SQL Server is not ready yet. Waiting..."
     sleep 5
 done
 
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Sqlserver123" -i /usr/config/init.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "Sqlserver123" -C -i /usr/config/init.sql
 
 wait
